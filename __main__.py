79e06670a2d69344a0ca42e9d5213544a07d57f1
Jaydon Reap <jaydonreap@gmail.com>
2017-09-11 22:08:42 -0400

    Added program entry point

---
+++ __main__.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+done = False
+
+#Main python entry point for the entire game
+#DO NOT RENAME THIS FILE OR PYTHON WILL NOT RUN IT
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        done = True
+        
+        pygame.display.flip()
