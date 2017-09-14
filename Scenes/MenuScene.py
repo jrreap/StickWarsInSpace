79f74669484571a00a7d44365957bc46d5fae825
Jaydon Reap <jaydonreap@gmail.com>
2017-09-14 09:03:43 -0400

    Added SceneManager and Scene classes to manage loading different 'game views'

---
+++ Scenes/MenuScene.py
@@ -0,0 +1,20 @@
+import pygame
+from SceneManager.SceneBase import SceneBase
+from ImageCache.ImageLoader import GetImage
+
+class MenuScene(SceneBase):
+    def __init__(self):
+        SceneBase.__init__(self)
+    
+    def ProcessInput(self, events, pressed_keys):
+        for event in events:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                self.SwitchToScene(None)
+                
+    def Update(self):
+        pass
+    
+    def Render(self, screen):
+        screen.fill((0, 0, 0))
+            
+        screen.blit(GetImage("./Images/background.jpg"), (0,0))
--- __main__.py
+++ __main__.py
@@ -1,36 +1,57 @@
 import pygame
 import os
-from UnitManagement.Unit import Unit
-from ImageCache.ImageLoader import GetImage
+from Scenes.MenuScene import MenuScene
 
-pygame.init()
-screen = pygame.display.set_mode((1200, 650))
-done = False
-x = 30
-y = 30
 
-clock = pygame.time.Clock()
-
-#Main entry point for the game
-#DO NOT RENAME THE FILE OR ELSE PYTHON WILL NOT RUN IT
-
-while not done:
-        for event in pygame.event.get():
+#MAIN METHOD
+def Start(width, height, fps, starting_scene):
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    done = False
+    x = 30
+    y = 30
+    
+    clock = pygame.time.Clock()
+    
+    active_scene = starting_scene
+    
+    #Main entry point for the game
+    #DO NOT RENAME THE FILE OR ELSE PYTHON WILL NOT RUN IT
+    
+    while active_scene != None:
+            pressed_keys = pygame.key.get_pressed()
+            
+            filtered_events = []
+            for event in pygame.event.get():
+                quit_attempt = False
+                
+                #Check if the input is a EXIT command or key
                 if event.type == pygame.QUIT:
-                        done = True
-        
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]: y -= 3
-        if pressed[pygame.K_DOWN]: y += 3
-        if pressed[pygame.K_LEFT]: x -= 3
-        if pressed[pygame.K_RIGHT]: x += 3
-        
-        screen.fill((0, 0, 0))
-        
-        screen.blit(GetImage("./Images/background.jpg"), (0,0))
-        
-        pygame.display.set_caption("Mongolian Space Stick Wars XD Special Day One Edition")
-        pygame.display.flip()
-        
-        #Make sure we stay at 60 frames per second
-        clock.tick(60)
+                    quit_attempt = True
+                elif event.type == pygame.KEYDOWN:
+                    alt_pressed = pressed_keys[pygame.K_LALT]
+                    if event.key == pygame.K_ESCAPE:
+                        quit_attempt = True
+                    elif event.key == pygame.K_F4 and alt_pressed:
+                        quit_attempt = True                            
+                
+                #Check if we are quitting
+                if quit_attempt:
+                    active_scene.Terminate()
+                else:
+                    filtered_events.append(event)
+                    
+            active_scene.ProcessInput(filtered_events, pressed_keys)
+            active_scene.Update()
+            active_scene.Render(screen)
+            
+            #Change the scene if the scene has changed
+            active_scene = active_scene.next
+            
+            pygame.display.set_caption("Mongolian Space Stick Wars XD Special Day One Edition")
+            pygame.display.flip()
+            clock.tick(fps)
+  
+#Start the game
+Start(1200, 650, 60, MenuScene())
+                   
