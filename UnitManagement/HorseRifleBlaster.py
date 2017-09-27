bca7aa4bdf6ca00fbadfe226d1c6cae7834af002
rohnkerman <31776738+rohnkerman@users.noreply.github.com>
2017-09-27 13:20:39 -0400

    Update HorseRifleBlaster.py
    finished

--- UnitManagement/HorseRifleBlaster.py
+++ UnitManagement/HorseRifleBlaster.py
@@ -1,3 +1,14 @@
 import pygame
 from UnitManagement.Unit import Unit
 
+class HorseRifleBlaster (Unit)
+
+unitclass = "Horse Rifle Blaster"
+damage = 50
+speed = 2
+health = 300
+unitcost = 200
+buildtime = 2
+attackrate = 1
+
+return None
