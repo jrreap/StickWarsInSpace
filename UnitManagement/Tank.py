6c54c157ac7624ef19fc8bb1920b2a2a10272cc0
rohnkerman <31776738+rohnkerman@users.noreply.github.com>
2017-09-27 13:27:30 -0400

    Update Tank.py

--- UnitManagement/Tank.py
+++ UnitManagement/Tank.py
@@ -2,12 +2,13 @@ import pygame
 from UnitManagement.Unit import Unit
 
 class Tank(Unit):
-    unitclass = "Space Tank"
-    damage = 500
-    speed = .5
-    health = 1600
-    unitcost = 500
-    buildtime = 5
-    attackrate = 5
+    self.unitclass = "Space Tank"
+    self.damage = 500
+    self.speed = .5
+    self.health = 1600
+    self.unitcost = 500
+    self.buildtime = 5
+    self.attackrate = 5
+    self.unitrange = 2
     
     return None
