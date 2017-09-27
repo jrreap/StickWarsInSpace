c854ebbb0671aa94538c9ff99f4d815d249f21c2
Jaydon Reap <jaydonreap@gmail.com>
2017-09-27 13:34:29 -0400

    Fixed HorseRifleBlaster.py

--- UnitManagement/HorseRifleBlaster.py
+++ UnitManagement/HorseRifleBlaster.py
@@ -3,13 +3,14 @@ from UnitManagement.Unit import Unit
 
 class HorseRifleBlaster (Unit):
 
-self.unitclass = "Horse Rifle Blaster"
-self.damage = 50
-self.speed = 2
-self.health = 300
-self.unitcost = 200
-self.buildtime = 2
-self.attackrate = 1
-self.unitrange = 4
+    def __init__(self):
+        unitclass = "Horse Rifle Blaster"
+        damage = 50
+        speed = 2
+        health = 300
+        unitcost = 200
+        buildtime = 2
+        attackrate = 1
+        unitrange = 4
 
 
