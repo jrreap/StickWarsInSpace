d40aad339fdd1cb6ac36d6ab6586dfa9835261c3
rohnkerman <31776738+rohnkerman@users.noreply.github.com>
2017-09-27 13:23:25 -0400

    Update Plane.py
    finished

--- UnitManagement/Plane.py
+++ UnitManagement/Plane.py
@@ -1,4 +1,14 @@
 import pygame
 from UnitManagement.Unit import Unit
 
-class SpacePlane
+class Plane (Unit)
+
+unitclass = "Space Plane"
+damage = 100
+speed = 2.5
+health = 200
+unitcost = 500
+buildtime = 5
+attackrate = 1
+
+return None
