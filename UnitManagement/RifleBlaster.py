c2cf7df6cf984868274d0b4de1d6dbbdd037ea7a
rohnkerman <31776738+rohnkerman@users.noreply.github.com>
2017-09-27 13:32:08 -0400

    Create RifleBlaster.py
    finished

---
+++ UnitManagement/RifleBlaster.py
@@ -0,0 +1,14 @@
+import pygame
+from UnitManagement.Unit import Unit
+
+class RifleBlaster (Unit):
+
+self.unitclass = "Rifle Blaster"
+self.damage = 100
+self.speed = 1
+self.health = 200
+self.unitcost = 200
+self.buildtime = 2
+self.attackrate = 1
+self.unitrange = 6
+
