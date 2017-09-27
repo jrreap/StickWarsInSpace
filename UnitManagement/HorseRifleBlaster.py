87ccfdf405ddfe151555c14dc30ffbe02f035592
rohnkerman <31776738+rohnkerman@users.noreply.github.com>
2017-09-27 13:30:00 -0400

    Update HorseRifleBlaster.py

--- UnitManagement/HorseRifleBlaster.py
+++ UnitManagement/HorseRifleBlaster.py
@@ -3,12 +3,13 @@ from UnitManagement.Unit import Unit
 
 class HorseRifleBlaster (Unit):
 
-unitclass = "Horse Rifle Blaster"
-damage = 50
-speed = 2
-health = 300
-unitcost = 200
-buildtime = 2
-attackrate = 1
+self.unitclass = "Horse Rifle Blaster"
+self.damage = 50
+self.speed = 2
+self.health = 300
+self.unitcost = 200
+self.buildtime = 2
+self.attackrate = 1
+self.unitrange = 4
 
 return None
