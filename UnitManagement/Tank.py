fa74a67459b071cd374d08868631a4361b4d2058
rohnkerman <31776738+rohnkerman@users.noreply.github.com>
2017-09-27 13:15:50 -0400

    tank finished
    not sure if there should be a return none

--- UnitManagement/Tank.py
+++ UnitManagement/Tank.py
@@ -2,4 +2,12 @@ import pygame
 from UnitManagement.Unit import Unit
 
 class SpaceTank(Unit):
+    unitclass = "Space Tank"
+    damage = 500
+    speed = .5
+    health = 1600
+    unitcost = 500
+    buildtime = 5
+    attackrate = 5
+    
     return None
