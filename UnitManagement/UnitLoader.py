c05e45f409f9ba08f737037c2b7431fb9cd6a6f9
Jaydon Reap <jaydonreap@gmail.com>
2017-09-29 09:33:39 -0400

    Added UnitLoader class

--- UnitManagement/UnitLoader.py
+++ UnitManagement/UnitLoader.py
@@ -6,7 +6,7 @@ from UnitManagement.SpaceRaider import SpaceRaider
 from UnitManagement.Tank import Tank
 from UnitManagement.Turret import Turret
 
-# Utility class that manages the loading of Unit information and the various components of that
+# Utility class that manages the loading of Unit information and the various components
 # to be used in the rendering of the unit on screen and the various functionality behind it
 
 class UnitLoader():
@@ -14,9 +14,6 @@ class UnitLoader():
     def __init__(self):
         self.UnitList = [Plane(), HorseRifleBlaster(), RifleBlaster(), SpaceRaider(), Tank(), Turret()]
         self.CreatedUnits = []
-
-
-
 
     # Searches through all the units and returns an instance of the unit by class
     def GetUnitByUnitClass(self, unitclass):
@@ -28,10 +25,17 @@ class UnitLoader():
                 print("COULD NOT FIND UNIT WITH CLASS " + unitclass)
                 return None
 
-    # Creates a Unit and adds it to the array of currently created units
-    def CreateUnit(self, unit):
+    # Instantiates a Unit and displays it to the screen
+    def InstantiateUnit(self, unit):
         self.CreatedUnits.append(unit)
 
     # Removes a Unit from the array of currently created unitsK
     def DeleteUnit(self, unit):
-        self.CreatedUnits.remove(unit)
+
+         for x in self.CreatedUnits:
+             if unit.unitid == x.unitid:
+                 self.CreatedUnits.remove(x)
+
+    # Returns the list of all currently existing units
+    def GetCreatedUnits(self):
+        return self.CreatedUnits
