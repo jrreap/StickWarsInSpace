822ede22246c6e389b8f84d0174b54d709e20ca5
Dalphus <31776800+Dalphus@users.noreply.github.com>
2017-11-28 13:50:45 -0500

    Animations!
    Basic infistructure for spritesheets

---
+++ UnitAnimations/Walk.py
@@ -0,0 +1,15 @@
+import pygame, sys
+import os
+
+class Walk():
+    def __init__(self):
+        self.wait = 0
+        self.frame = 0
+    def update(self):
+        if self.wait == 0:
+            self.frame += 90
+            if self.frame == 90*8:
+                self.frame = 0
+        self.wait += 1
+        if self.wait == 6:
+            self.wait = 0
