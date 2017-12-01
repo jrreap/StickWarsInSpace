5fd2ea31284f8523c44de72942d795f100002875
rohnkerman <31776738+rohnkerman@users.noreply.github.com>
2017-11-30 22:45:22 -0500

    added adius son lore

--- Scenes/Lore/SpaceMongolianLoreAddison.py
+++ Scenes/Lore/SpaceMongolianLoreAddison.py
@@ -1,1 +1,14 @@
+import pygame
+from Scenes.SceneBase import SceneBase
+from Scenes.GameScene import GameScene
+from ImageCache.ImageLoader import getImage
 
+class SpaceMongolianLoreAddison (SceneBase):
+
+    def Update(self):
+        pass
+
+    def Render (self, screen):
+        screen.fill((0, 0, 0))
+
+        screen.blit(GetImage("./Images/addisonlore.jpg"), (0,0))
