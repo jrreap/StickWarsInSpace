a0a8c6cd8507a534392bd1754ed10ec9eebb5e41
rohnkerman <31776738+rohnkerman@users.noreply.github.com>
2017-11-30 23:03:38 -0500

    added kramer lore

--- Scenes/Lore/SpaceMongolianLoreKraymer.py
+++ Scenes/Lore/SpaceMongolianLoreKraymer.py
@@ -1,1 +1,14 @@
+import pygame
+from Scenes.SceneBase import SceneBase
+from Scenes.GameScene import GameScene
+from ImageCache.ImageLoader import getImage
 
+class SpaceMongolianLoreKraymer (SceneBase):
+
+    def Update(self):
+        pass
+
+    def Render (self, screen):
+        screen.fill((0, 0, 0))
+
+        screen.blit(GetImage("./Images/kramerlore.jpg"), (0,0))
