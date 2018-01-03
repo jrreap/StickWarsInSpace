09dab03b0275f79ae3c23c63ac67df32237aa36e
Dalphus <31776800+Dalphus@users.noreply.github.com>
2018-01-03 10:24:22 -0500

    stuff

--- UnitAnimations/Walk.py
+++ UnitAnimations/Walk.py
@@ -2,22 +2,24 @@ import pygame, sys
 import os
 
 class Walk():
-    def __init__(self):
+    def __init__(self,w,nf):
         self.wait = 0
         self.frame = 0
+        self.width = w
+        self.numframes = nf
     def nextFrame(self):
         if self.wait == 0:
-            self.frame += 90
-            if self.frame >= 90*7:
+            self.frame += width
+            if self.frame >= width*(numframes):
                 self.frame = 0
         self.wait += 1
         if self.wait == 4:
             self.wait = 0
     def prevFrame(self):
         if self.wait == 0:
-            self.frame -= 90
+            self.frame -= width
             if self.frame <= 0:
-                self.frame = 90*6
+                self.frame = width*(numframes-1)
         self.wait += 1
         if self.wait == 4:
             self.wait = 0
