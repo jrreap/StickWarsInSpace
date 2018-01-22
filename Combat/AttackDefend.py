ef9a0993cac8c76d798933f419ac592d9e5da01e
addison171 <addison171@gmail.com>
2018-01-22 08:18:49 -0500

    Now you can lose the game!!

--- Combat/AttackDefend.py
+++ Combat/AttackDefend.py
@@ -19,8 +19,98 @@ class AttackDefend():
         return detect
 
     @classmethod
-    def Attack(cls, Friendlies, Enemies, AttackRate, OGSpeed=0):
-        x = 0
+    def Attack(cls, Friendlies, Enemies, EnemyList, Rate, OGSpeed=0, EOGSpeed=0):
+        if(Friendlies.speed!=0):
+            OGSpeed = Friendlies.speed
+        Friendlies.speed = 0
+        if((Rate)>29):
+            Enemies.DamageUnit(Friendlies.damage)
+            if(Enemies.health<1):
+                UnitSpawner.DeleteUnit(Enemies)
+                CurrencyManagement.AddMoonCrystals(100)
+
+    @classmethod
+    def EAttack(cls, Friendlies, Enemies, EnemyList, Rate, OGSpeed=0, EOGSpeed=0):
+        if(Friendlies.speed!=0):
+            OGSpeed = Friendlies.speed
+        Friendlies.speed = 0
+        if((Rate)>29):
+            Enemies.DamageUnit(Friendlies.damage)
+            if(Enemies.health<1):
+                UnitLoader.DeleteUnit(Enemies)
+                CurrencyManagement.AddMoonCrystals(100)
+            
+    @classmethod
+    def UnitsAttack(cls, Units, Enemies, AttackRate, EAttackRate):
+        # You attack
+        fe = 0
+        fu = 0
+        while(fu<len(Units)):
+            while(fe<len(Enemies)):
+                detect = AttackDefend.InRange(Units[fu], Enemies[fe])
+                if(detect==True):
+                    AttackDefend.Attack(Units[fu], Enemies[fe], Enemies, AttackRate)
+                fe = fe + 1
+            fu = fu + 1
+        #if AttackRate>30:
+         #   AttackRate=0
+        #else:
+         #   AttackRate += 1
+
+        # AI attacks
+        ee = 0
+        eu = 0
+        while(ee<len(Enemies)):
+            while(eu<len(Units)):
+                detect = AttackDefend.InRange(Enemies[ee], Units[eu])
+                if(detect):
+                    AttackDefend.EAttack(Enemies[ee], Units[eu], Units, EAttackRate)
+                eu = eu + 1
+            ee = ee + 1
+      #  if (EAttackRate > 30):
+       #     EAttackRate=0
+        #else:
+         #   EAttackRate += 1
+
+        #Make units move again
+        a=0
+        b=0
+        detect = False
+        while(a<len(Enemies)):
+            while(b<len(Units)):
+                detect = AttackDefend.InRange(Enemies[a], Units[b])
+                if(detect):
+                    break
+                b += 1
+            if(detect==False):
+                Enemies[a].speed = Enemies[a].OGSpeed
+            detect = False
+            a += 1
+        a=0
+        b=0
+        while(a<len(Units)):
+            while(b<len(Enemies)):
+                detect = AttackDefend.InRange(Units[a], Enemies[b])
+                if(detect):
+                    break
+                b += 1
+            if(detect==False):
+                Units[a].speed = Units[a].OGSpeed
+            detect = False
+            a += 1
+        a=0
+        b=0
+        if(len(Units)==0):
+            while(b<len(Enemies)):
+                Enemies[b].speed = Enemies[b].OGSpeed
+                b += 1
+        if(len(Enemies)==0):
+            while(a<len(Units)):
+                Units[a].speed = Units[b].OGSpeed
+                a += 1
+                
+        
+'''        x = 0
         y = 0
         Rate = AttackRate
         while(x<len(Friendlies)):
@@ -40,13 +130,15 @@ class AttackDefend():
                               Unit1.speed = 0
                               Unit2.DamageUnit(Unit1.damage)                         
                               if Unit2.health<1:
+                                  Unit2.damage = 0
                                   UnitSpawner.DeleteUnit(Unit2)
                                   CurrencyManagement.AddMoonCrystals(100)
                                   Unit1.speed = OGSpeed
                   if(Unit2.health<1):
                       Unit1.speed = OGSpeed
 
-    @classmethod
+'''          
+'''    @classmethod
     def EAttack(cls, Enemies, Friendlies, EAttackRate, OGSpeed=0):
         x = 0
         y = 0
@@ -68,13 +160,23 @@ class AttackDefend():
                               Unit1.speed = 0
                               Unit2.DamageUnit(Unit1.damage)                         
                               if Unit2.health<1:
+                                  Unit2.damage = 0
                                   UnitLoader.DeleteUnit(Unit2)
                                   Unit1.speed = OGSpeed
+                                  d = 0
+                                  s = 0
+                                  while(d<len(Enemies)):
+                                      while(s<len(Friendlies)):
+                                          still = AttackDefend.InRange(Enemies[d], Friendlies[s])
+                                          if(still==False):
+                                              Enemies[d].speed = OGSpeed
+                                          s = s+1
+                                      d = d+1
                   if(Unit2.health<1):
                       Unit1.speed = OGSpeed
-                  
+             
 
-    
+   ''' 
 """@classmethod
     def Attack(cls, Friendlies, Enemies):
         x = 0
--- Combat/WinCon.py
+++ Combat/WinCon.py
@@ -2,6 +2,7 @@ import pygame
 from Scenes import*
 from UnitManagement import*
 from UnitManagement.UnitLoader import UnitLoader
+from UnitManagement.UnitSpawner import UnitSpawner
 
 class WinCon():
     @classmethod
@@ -18,7 +19,7 @@ class WinCon():
         x = 0
         BaseHealth = CurrentHealth
         if(base == 1):
-            basePos = 3531
+            basePos = 9
         if(base == 0):
             basePos = 3531
         while(x<len(units)):
@@ -28,6 +29,27 @@ class WinCon():
             if abs(CurrentUnit.xpos-basePos)<10:
                 Health = WinCon.AttackPlayer(BaseHealth, CurrentUnit.health)
                 UnitLoader.DeleteUnit(CurrentUnit)
+                x = len(units) + 1
+                print Health
+                return Health
+            else:
+                return BaseHealth
+
+    @classmethod
+    def ReachedEPlayer(cls, units, base, CurrentHealth=1000):
+        x = 0
+        BaseHealth = CurrentHealth
+        if(base == 1):
+            basePos = 9
+        if(base == 0):
+            basePos = 3531
+        while(x<len(units)):
+            CurrentUnit = units[x]
+            x = x+1
+            UnitRange = CurrentUnit.unitrange
+            if abs(CurrentUnit.xpos-basePos)<10:
+                Health = WinCon.AttackPlayer(BaseHealth, CurrentUnit.health)
+                UnitSpawner.DeleteUnit(CurrentUnit)
                 x = len(units) + 1
                 print Health
                 return Health
