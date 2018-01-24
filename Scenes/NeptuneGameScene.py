caf36bf38859c2e42eafc92ba2d020bfb38ac70c
BuildTools <jaydonreap@gmail.com>
2018-01-23 22:04:40 -0500

    Added Base and Enemy Base health bars

--- Scenes/NeptuneGameScene.py
+++ Scenes/NeptuneGameScene.py
@@ -9,6 +9,7 @@ from UI.Button import Button
 from UI.Bar import Bar
 from UI.StatBar import StatBar
 from UI.ToggleMenu import ToggleMenu
+from UI.Text import Text
 from Combat.Detect import Detect
 from Combat.AttackDefend import AttackDefend
 from Combat.WinCon import WinCon
@@ -78,6 +79,14 @@ class NeptuneGameScene(SceneBase):
         self.buildmenutoggle = False
 
         self.buildqueue = StatBar(" ", (1090, 635), size=(200, 20), bg=(176, 185, 186), fg=(109, 177, 255))
+
+        self.basehealth = StatBar(" ", (80, 45), size=(150, 20), bg=(176, 185, 186), fg=(219, 40, 0))
+
+        self.ebasehealth = StatBar(" ", (80, 15), size=(150, 20), bg=(176, 185, 186), fg=(109, 177, 255))
+
+        self.text = Text(15, 80, "Base Health", bold=True, fontSize=18)
+
+        self.etext = Text(45, 80, "Enemy Base Health", bold=True, fontSize=18)
 
         self.buildmenu.AddButton(self.buildhorserifleblaster)
         self.buildmenu.AddButton(self.buildrifleblaster)
@@ -164,6 +173,10 @@ class NeptuneGameScene(SceneBase):
                 
         self.cu = UnitLoader.GetCreatedUnits()
         self.ce = UnitSpawner.GetCreatedUnits()
+
+        self.basehealth.SetFillPercentage(self.Health, 1000)
+
+        self.ebasehealth.SetFillPercentage(self.EHealth, 1000)
 
         #Generate Money
         if(len(self.cu)>0):
@@ -283,6 +296,12 @@ class NeptuneGameScene(SceneBase):
         self.buildqueue.Draw(screen)
         self.buildmenu.Draw(screen)
         self.openmenu.Draw(screen)
+
+        self.basehealth.Draw(screen)
+        self.ebasehealth.Draw(screen)
+
+        self.text.Draw(screen)
+        self.etext.Draw(screen)
 
     # Cleanup function
     def Terminate(self):
